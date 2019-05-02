519af7816bb851ffc8df6961721b3c890551c167
Adi Saripuloh <adisaripuloh@gmail.com>
2019-05-02 13:30:46 +0700

    update: split dashboard layouts to partials

---
+++ app/Modules/Dashboard/Resources/Views/partials/__user-panel.blade.php
@@ -0,0 +1,10 @@
+<!-- Sidebar user panel -->
+<div class="user-panel">
+	<div class="pull-left image">
+		<img src="{{ asset('images/vendor/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
+	</div>
+	<div class="pull-left info">
+		<p>{{ userInfo()->name }}</p>
+		<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+	</div>
+</div>
